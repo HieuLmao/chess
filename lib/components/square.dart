b8058dfd714bb5b392155608af1a702f6a9dbf9d
hieu nguyen <hieumi0772@gmail.com>
2023-06-10 01:48:44 +0700

    color

--- lib/components/square.dart
+++ lib/components/square.dart
@@ -5,22 +5,35 @@ import 'package:social_media_app/styles/styles.dart';
 class Square extends StatelessWidget {
   final ChessPiece? piece;
   final bool isWhite;
+  final bool isSelected;
+  final void Function()? onTap;
   const Square({
     super.key,
+    required this.onTap,
+    required this.isSelected,
     required this.piece,
     required this.isWhite,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: isWhite ? foregroundColor : backgroundColor,
-      child: piece != null
-          ? Image.asset(
-              piece!.imagePath,
-              color: piece!.isWhite ? Colors.white : Colors.black,
-            )
-          : null,
+    Color? squareColor;
+    if (isSelected) {
+      squareColor = Colors.green;
+    } else {
+      squareColor = isWhite ? foregroundColor : backgroundColor;
+    }
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        color: squareColor,
+        child: piece != null
+            ? Image.asset(
+                piece!.imagePath,
+                color: piece!.isWhite ? Colors.white : Colors.black,
+              )
+            : null,
+      ),
     );
   }
 }
