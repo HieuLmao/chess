f3bf9a3c38a9439da2462966fed545685adbb13c
hieu nguyen <hieumi0772@gmail.com>
2023-06-10 01:05:43 +0700

    square

--- lib/components/square.dart
+++ lib/components/square.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:social_media_app/components/piece.dart';
+import 'package:social_media_app/styles/styles.dart';
 
 class Square extends StatelessWidget {
+  final ChessPiece? piece;
   final bool isWhite;
   const Square({
     super.key,
+    required this.piece,
     required this.isWhite,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: isWhite ? Colors.grey[200] : Colors.grey[500],
+      color: isWhite ? foregroundColor : backgroundColor,
+      child: piece != null
+          ? Image.asset(
+              piece!.imagePath,
+              color: piece!.isWhite ? Colors.white : Colors.black,
+            )
+          : null,
     );
   }
 }
